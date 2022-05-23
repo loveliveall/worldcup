3afc2a696c661f6899a57de75869da2db61b7090
LoveLive <lovelivefam@gmail.com>
2022-05-23 14:52:02 +0900

    Add chakra-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import App from '@/App';
 
+function Root() {
+  return (
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  );
+}
+
 ReactDOM.render(
-  <App />,
+  <Root />,
   document.getElementById('app'),
 );
